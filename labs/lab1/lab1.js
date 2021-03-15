b9e9b3898f1aaca254875099de6b893c19ac3ca3
IsaacLowe4 <ijlowe@iu.edu>
2021-03-14 20:04:44 -0400

    Lab 1 Complete

---
+++ labs/lab1/lab1.js
@@ -0,0 +1,18 @@
+function setup() {
+    var yellow = "#ffdd00";
+    var mint = "#CBFFFA"
+    
+    createCanvas(400, 400);
+    background(mint);
+
+    fill(yellow);
+    circle (200,200,300);
+
+    arc(200, 255, 150, 125, 0, PI, CHORD);
+
+    line(125, 100, 190, 200);
+    line(125, 200, 190, 100);
+    line(275, 100, 210, 200);
+    line(275, 200, 210, 100);
+}
+  
