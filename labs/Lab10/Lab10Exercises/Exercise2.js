2758196af4096e8910a9f834d92d5bf535c0d91f
IsaacLowe4 <ijlowe@iu.edu>
2021-04-30 00:57:55 -0400

    this took forever.

--- labs/Lab10/Lab10Exercises/Exercise2.js
+++ labs/Lab10/Lab10Exercises/Exercise2.js
@@ -1,7 +1,36 @@
-let currentColor = document.getElementById("currentColor").innerHTML;
+let red = 0;
+let green = 0;
+let blue = 0;
 
 
+function blueCalc(event){
 
+ let response = event.target.getAttribute("data-response");
+ let newBlue = parseInt(response);
+ blue = blue + newBlue
+ console.log(blue);
+ onChange();
+}
+
+function redCalc(event){
+
+   let response = event.target.getAttribute("data-response");
+   let newRed = parseInt(response);
+   red = red + newRed
+   console.log(red);
+   onChange();
+  }
+
+  function greenCalc(event){
+
+   let response = event.target.getAttribute("data-response");
+   let newGreen = parseInt(response);
+   green = green + newGreen
+   console.log(green);
+   onChange();
+  }
+    
 function onChange(){
-   document.getElementById("currentColor").innerHTML = "yes"
+ document.getElementById("currentColor").innerHTML= "RGB("+red+","+green+","+blue+")";
+ document.getElementById("colorRect").style.backgroundColor = "rgb("+red+","+green+","+blue+")"
 }
